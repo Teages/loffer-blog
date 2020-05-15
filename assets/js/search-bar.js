e0558d517a968b1bf97ee12fd0a23a72142410fc
Teages <Teages@teages.xyz>
2020-05-15 22:26:09 +0800

    part1

--- assets/js/search-bar.js
+++ assets/js/search-bar.js
@@ -1,9 +1,20 @@
-function showSearchBar ()
-{
+jQuery.easing.def = "easeInQuart";
 
-}
-
-function hideSearchBar ()
-{
-
+$(document).ready(function () {
+    $('#btn-search').click(function () {
+        $('#search-bar').show();
+        $('#btn-search').hide();
+        $('#search-bar').animate({ width: '400px' });
+        $('#search-clean').show();
+    });
+});
+function closeSearchBar() {
+    if (document.getElementById("btn-search").value == "") {
+        $('#search-clean').animate({ display: 'none' });
+        $('#search-bar').animate({ width: '40px' });
+        $('#search-bar').animate({ display: 'none' });
+        $('#btn-search').animate({ display: 'inline' });
+    } else {
+        document.getElementById("btn-search").value = "";
+    }
 }
